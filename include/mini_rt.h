9ac4a7ee82ecc8a8585bd076c1dfb6060356a0b7
malee <martinleex7568@hotmail.com>
2024-12-05 18:23:47 +0800

    Initial commit

---
+++ include/mini_rt.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_rt.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malee <malee@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 17:49:00 by malee             #+#    #+#             */
+/*   Updated: 2024/12/05 17:49:44 by malee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINI_RT
+# define MINI_RT
+
+# include <fcntl.h>
+# include <math.h>
+# include <mlx.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
