c80b688740cee7ac02a38fa40038c9de92ec4d0b
SeanYeoTE <tseensean@gmail.com>
2025-01-05 14:13:44 +0800

    notes init

---
+++ renderplane/plane.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plane.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <seayeo@42.sg>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/05 14:06:36 by seayeo            #+#    #+#             */
+/*   Updated: 2025/01/05 14:11:00 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PLANE_H
+# define PLANE_H
+
+typedef struct s_img
+{
+	void	*img_ptr;
+	char	*pixels_ptr;
+	int		bpp;
+	int		endian;
+	int		line_len;
+}	t_img;
+
+typedef struct s_plane
+{
+    uin
+}
+# endif
---
+++ renderplane/renderplane.c
@@ -0,0 +1,9 @@
+/*
+to render a plane successfully
+
+data values required:
+camera vectors
+camera view vectors
+camera fov
+
+plane struct; */
